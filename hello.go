b84df989a38c8dba902ef1f189aa2bcd30d27b45
anders <anders_b28@hotmail.com>
2021-05-10 17:05:08 +0200

    router test

--- hello.go
+++ hello.go
@@ -1,25 +1,63 @@
 package main
 
-import "fmt"
-import "github.com/m0stly1/playground/model"
+
+import (
+	"github.com/m0stly1/playground1/model"
+	"github.com/gorilla/mux"
+    "log"
+    "net/http"
+    "fmt"
+    "time"
+    )
 
 func main() {
+	
+	r := mux.NewRouter()
 
-	fmt.Println(GetMessage("0"))
-	msg_Delete("0");
-	fmt.Println(GetMessage("0"))
+	r.HandleFunc("/api/message", addMessage).Methods("POST")
+	r.HandleFunc("/api/message/{id:[0-9]+}", getMessage).Methods("GET")
+	r.HandleFunc("/api/message", updateMessage).Methods("PUT")
+	r.HandleFunc("/api/message", deleteMessage).Methods("DELETE")
 
 
-	m := &model.Message{
-		Title : "hej hej",
-    	Content : "new body vettu",
-	}
+	srv := &http.Server{
+        Handler:      r,
+        Addr:         "127.0.0.1:8000",
+        // Good practice: enforce timeouts for servers you create!
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
 
-	create(m)
+    log.Fatal(srv.ListenAndServe())
 
-	fmt.Println(GetMessage("3"))
 }
 
+
+func getMessage(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusOK)
+	id := mux.Vars(r)["id"]
+
+	if !id {
+
+	}
+
+
+
+    fmt.Fprintf(w, id)
+}
+
+func addMessage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "post", r.URL.Path[1:])
+}
+
+func updateMessage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "put", r.URL.Path[1:])
+}
+
+func deleteMessage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "delete", r.URL.Path[1:])
+}
 
 func create(m *model.Message){
 
